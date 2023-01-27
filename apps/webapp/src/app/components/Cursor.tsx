3a66648dbe7e18a44c1af4fd4b9de99f32834134
Tar-Tarus <loubia312@gmail.com>
2023-01-27 14:00:32 +0100

    feat: add custom cursor

---
+++ apps/webapp/src/app/components/Cursor.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import * as framerMotion from "framer-motion";
+import { useEffect, useState } from "react";
+
+const CURSOR_VARIANTS = ["default", "hover", "down"] as const;
+
+type CursorVariant = (typeof CURSOR_VARIANTS)[number];
+
+const Cursor = () => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const [cursorVariant, setCursorVariant] = useState<CursorVariant>("default");
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const handleMouseMove = (event: MouseEvent) => {
+      setMousePosition(() => ({
+        x: event.clientX,
+        y: event.clientY,
+      }));
+    };
+
+    const handleMouseDown = () => {
+      setCursorVariant(() => "down");
+    };
+
+    const handleMouseUp = () => {
+      setCursorVariant(() => "default");
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mousedown", handleMouseDown);
+    window.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mousedown", handleMouseDown);
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, []);
+
+  const variants: framerMotion.Variants = {
+    default: {
+      x: mousePosition.x - 17,
+      y: mousePosition.y - 17,
+    },
+    down: {
+      cursor: "help",
+      x: mousePosition.x - 17,
+      y: mousePosition.y - 17,
+    },
+  };
+
+  const centerVariants: framerMotion.Variants = {
+    default: {
+      r: 2,
+    },
+    down: {
+      r: 4,
+    },
+  };
+
+  const innerVariants: framerMotion.Variants = {
+    default: {
+      r: 6,
+    },
+    down: {
+      r: 12,
+    },
+  };
+
+  return (
+    <framerMotion.motion.div
+      className="pointer-events-none fixed top-0 left-0 z-50 h-[34px] w-[34px]"
+      variants={variants}
+      animate={cursorVariant}
+      dragElastic={0}
+    >
+      <framerMotion.motion.svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="34"
+        height="34"
+        fill="none"
+        viewBox="0 0 34 34"
+      >
+        {/* Outer */}
+        <framerMotion.motion.circle
+          cx="17"
+          cy="17"
+          r="15.5"
+          stroke="#FF7218"
+        ></framerMotion.motion.circle>
+        {/* Center */}
+        <framerMotion.motion.circle
+          variants={centerVariants}
+          animate={cursorVariant}
+          transition={{
+            duration: 0.2,
+            ease: "easeInOut",
+            times: [0, 0.2, 0.5, 0.8, 1],
+          }}
+          cx="17"
+          cy="17"
+          r="2"
+          fill="#FF7218"
+        ></framerMotion.motion.circle>
+        {/* Inner */}
+        <framerMotion.motion.circle
+          variants={innerVariants}
+          animate={cursorVariant}
+          transition={{
+            duration: 0.2,
+            ease: "easeInOut",
+            times: [0, 0.2, 0.5, 0.8, 1],
+          }}
+          cx="17"
+          cy="17"
+          r="6"
+          stroke="#FF7218"
+        ></framerMotion.motion.circle>
+        {/* Cross */}
+        <framerMotion.motion.path
+          stroke="#FF7218"
+          strokeWidth="0.5"
+          d="M0 17h34M17 0v34"
+        ></framerMotion.motion.path>
+      </framerMotion.motion.svg>
+    </framerMotion.motion.div>
+  );
+};
+
+export default Cursor;
--- apps/webapp/src/app/layout.tsx
+++ apps/webapp/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Suspense } from "react";
 import { ClientProvider } from "../client/trpc";
 import WagmiProvider from "../client/wagmi";
 import "../styles/globals.css";
+import Cursor from "./components/Cursor";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
@@ -21,7 +22,7 @@ function RootLayout({
   return (
     <html lang="en" className={`${customFont.variable} mono font-mono lg:p-6`}>
       {/* <html lang="en" className={customFont.className + " mono"}> */}
-      <body className="mx-auto flex flex-col bg-dark-background px-3 pt-14 pb-20 lg:rounded-2xl lg:px-36">
+      <body className="relative mx-auto flex flex-col bg-dark-background px-3 pt-14 pb-20 lg:rounded-2xl lg:px-36">
         <Suspense fallback={<div>Loading...</div>}>
           <ClientProvider>
             <WagmiProvider>
@@ -30,6 +31,7 @@ function RootLayout({
               <Footer />
             </WagmiProvider>
           </ClientProvider>
+          <Cursor />
         </Suspense>
       </body>
     </html>
