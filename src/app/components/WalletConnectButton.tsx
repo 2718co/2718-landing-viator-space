d131ca4aa292f95f71e13ff1d6f2ca9ed74f57c3
ferrodri <f.rodriguez.hervias@gmail.com>
2023-09-05 16:02:49 +0200

    css fix

--- src/app/components/WalletConnectButton.tsx
+++ src/app/components/WalletConnectButton.tsx
@@ -1,21 +1,31 @@
 'use client';
 
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useConnectModal } from '@rainbow-me/rainbowkit';
 import { clsx } from 'clsx';
 import React from 'react';
-
 interface IWalletConnectButtonProps {
     className?: string;
 }
 
 const WalletConnectButton = (props: IWalletConnectButtonProps): JSX.Element => {
+    const { openConnectModal } = useConnectModal();
+
+    if (!openConnectModal) {
+        return <></>;
+    }
 
     return (
-        <div className={clsx('mono rounded-2xl border-2 border-highlight px-8 py-4 font-mono text-2xl tracking-wide justify-center flex', props.className)}>
-            <ConnectButton />
-        </div> 
+        <button
+            onClick={openConnectModal}
+            type="button"
+            className={clsx(
+                'mono rounded-2xl border-2 border-highlight px-8 py-4 font-mono text-2xl tracking-wide justify-center flex hover:bg-hover-rectangle',
+                props.className
+            )}
+        >
+            Connect Wallet
+        </button>
     );
 };
 
 export default WalletConnectButton;
-
