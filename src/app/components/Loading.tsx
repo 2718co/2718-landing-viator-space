ac3436f82307e4d7569166e000314c401bb8312f
ferrodri <f.rodriguez.hervias@gmail.com>
2023-09-13 19:28:31 +0200

    loading should be golden

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -6,7 +6,7 @@ export const Loading = ({ width = 'w-8' }) => {
             <div role="status">
                 <svg
                     aria-hidden="true"
-                    className={`mr-2 inline ${width} animate-spin fill-blue-600 text-gray-200 dark:text-gray-200`}
+                    className={`mr-2 inline ${width} animate-spin fill-highlight text-gray-200 dark:text-gray-200`}
                     viewBox="0 0 100 101"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
