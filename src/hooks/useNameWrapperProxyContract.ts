c54a962f5389460e0f4636a4a9fcbaad0f838f39
ferrodri <f.rodriguez.hervias@gmail.com>
2023-09-20 17:38:57 +0200

    add deployed contract

--- src/hooks/useNameWrapperProxyContract.ts
+++ src/hooks/useNameWrapperProxyContract.ts
@@ -8,6 +8,7 @@ export function useNameWrapperProxyContract(): `0x${string}` | undefined {
     const { chain } = useNetwork();
 
     const nameWrapperProxyContract: NameWrapperProxyMap = {
+        1: '0x55904ec64cedf31239bffbd63d956770ec172a8f',
         5: '0x96f88d45a64470e9a4d990cef6dd6943de131431'
     };
 
