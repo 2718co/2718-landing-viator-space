f585e2c81e61cbd2cf8c7e4aaf6b3b61fb2fc1ce
ferrodri <f.rodriguez.hervias@gmail.com>
2023-09-22 12:28:25 +0200

    try mobile design

--- src/app/components/landing/GlowButton.tsx
+++ src/app/components/landing/GlowButton.tsx
@@ -1,8 +1,11 @@
 const GlowButton = () => {
     return (
-        <div className="rounded-2xl border-2 border-highlight bg-transparent px-14 py-4">
-            <span className="text-2xl font-bold uppercase text-white">Enter the viator program</span>
-        </div>
+        <a
+            href="https://app.2718.co/"
+            className="z-10 w-[90vw] flex justify-center mt-4 sm:mt-auto mx-auto sm:w-auto rounded-2xl border-2 border-highlight bg-transparent sm:px-14 py-4"
+        >
+            <span className="sm:text-2xl font-bold uppercase text-white">Enter viator program</span>
+        </a>
     );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import LandingPageBackground from './components/landing/LandingPageBackground';
 import Logo2718 from './components/landing/Logo2718';
 import ViatorInline from './components/landing/ViatorInline';
 
-
 const SceneDesktop = dynamic(() => import('./components/landing/SceneDesktop'), {
     ssr: true
 });
@@ -30,8 +29,8 @@ const HomePage = () => {
             <LandingPageBackground />
             <div className="relative m-auto flex max-w-2xl flex-1 flex-col items-center justify-between">
                 <Logo />
-                <div className="relative h-72 w-full overflow-visible">
-                    <div className="absolute -left-36 top-4 flex flex-col text-[6px] text-white">
+                <div className="relative sm:h-72 w-full overflow-visible">
+                    <div className="absolute left-14 sm:-left-36 top-4 flex flex-col text-[6px] text-white">
                         <span>Location: TRAPPIST Solar System</span>
                         <span>Edl COORDINATES *ACQUIRED*</span>
                         <span>2718 OPERATED</span>
@@ -44,7 +43,7 @@ const HomePage = () => {
                     <div className="hidden lg:block h-full w-full overflow-visible">
                         <SceneDesktop className="w-screen"></SceneDesktop>
                     </div>
-                    <div className="absolute -left-32 bottom-4 flex flex-col text-[6px] text-white">
+                    <div className="absolute right-12 -z-10 sm:-left-32 top-8 sm:bottom-4 flex flex-col text-[6px] text-white">
                         <span>RA: 23h 06m 29.368s</span>
                         <span>DEC: -05° 02&apos; 29.04&quote;</span>
                         <span>m: 18.798 ± 0.082</span>
@@ -53,41 +52,50 @@ const HomePage = () => {
                         <span>AND TO INSPIRE</span>
                     </div>
                 </div>
-                <Image
-                    src='/viator-logo.svg'
-                    alt='Viator Logo'
-                    height="46"
-                    width="432"
-                    className='px-8 w-full'
-                />
+                <Image src="/viator-logo.svg" alt="Viator Logo" height="46" width="432" className="px-8 w-full" />
                 <div className="flex flex-col items-center gap-8 text-xs uppercase text-white">
                     <span className="inline-flex items-baseline">
                         Welcome to the <ViatorInline /> Space program
                     </span>
-                    <span className="flex flex-col items-center">
-                        <span className="inline-flex items-baseline text-center">
-                            The <ViatorInline /> program, also known as project <ViatorInline />, is the first human
-                            spaceflight
+                    <div className="block px-4 text-center sm:hidden">
+                        <span>
+                            The Viator program, also known as project Viator, is the first human spaceflight program
+                            carried out by the 2718 Collective. Working to reach the trappist solar system, our plan is
+                            to bring back inspiration to humankind, and to test the limits of what is possible.
                         </span>
-                        <span className="inline-flex items-baseline text-center">
-                            program carried out by the 2718 Collective. Working to reach the
+                        <br></br>
+                        <br></br>
+                        <span>Do not fear the unknown. Explore.</span>
+                        <br></br>
+                        <br></br>
+                    </div>
+                    <div className="hidden sm:flex flex-col items-center">
+                        <span className="flex flex-col items-center">
+                            <span className="inline-flex items-baseline text-center">
+                                The <ViatorInline /> program, also known as project <ViatorInline />, is the first human
+                                spaceflight
+                            </span>
+                            <span className="inline-flex items-baseline text-center">
+                                program carried out by the 2718 Collective. Working to reach the
+                            </span>
+                            <span className="inline-flex items-baseline text-center">
+                                trappist solar system, our plan is to bring back inspiration to
+                            </span>
+                            <span className="inline-flex items-baseline text-center">
+                                humankind, and to test the limits of what is possible.
+                            </span>
                         </span>
-                        <span className="inline-flex items-baseline text-center">
-                            trappist solar system, our plan is to bring back inspiration to
-                        </span>
-                        <span className="inline-flex items-baseline text-center">
-                            humankind, and to test the limits of what is possible.
-                        </span>
-                    </span>
-                    <span>do not fear the unknown. Explore.</span>
+                        <br></br>
+                        <br></br>
+                        <span className="text-center">Do not fear the unknown. Explore.</span>
+                    </div>
                 </div>
                 <Logo2718 />
             </div>
-            <div className="relative mt-28 flex flex-col items-end justify-between px-12 lg:flex-row">
+            <GlowButton />
+            <div className="hidden sm:flex relative mt-28 flex-col items-end justify-between px-12 lg:flex-row">
                 <div className="flex-1"></div>
-                <div className="flex-shrink flex-grow-0">
-                    <GlowButton />
-                </div>
+                <div className="flex-shrink flex-grow-0"></div>
                 <div className="flex flex-1 justify-end">
                     <Countdown />
                 </div>
