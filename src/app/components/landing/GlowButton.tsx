44157dcbc562ce0ceee4dfdd2492d95af21bbaca
ferrodri <f.rodriguez.hervias@gmail.com>
2023-09-22 13:03:55 +0200

    minor fix button

--- src/app/components/landing/GlowButton.tsx
+++ src/app/components/landing/GlowButton.tsx
@@ -2,7 +2,7 @@ const GlowButton = () => {
     return (
         <a
             href="https://app.2718.co/"
-            className="fixed bg-[#1a1a1a] bottom-4 right-[5vw] z-10 w-[90vw] flex justify-center mt-4 sm:mt-auto mx-auto sm:w-auto rounded-2xl border-2 border-highlight bg-transparent sm:px-14 py-4"
+            className="fixed bg-[#1a1a1a] sm:bg-transparent bottom-4 right-[5vw] z-10 w-[90vw] flex justify-center mt-4 sm:mt-auto mx-auto sm:w-auto rounded-2xl border-2 border-highlight sm:px-14 py-4"
         >
             <span className="sm:text-2xl font-bold uppercase text-white">Enter viator program</span>
         </a>
