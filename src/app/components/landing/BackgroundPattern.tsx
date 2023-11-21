52a14199ba29931977a8b7007aad4cf304ce64a5
ferrodri <f.rodriguez.hervias@gmail.com>
2023-11-21 17:23:39 +0100

    trigger deploy

--- src/app/components/landing/BackgroundPattern.tsx
+++ src/app/components/landing/BackgroundPattern.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const BackgroundPattern = () => {
-    return (
-        <div 
-            className="absolute inset-0 bg-no-repeat bg-bottom bg-cover bg-[url('/background.svg')]" 
-        />
-    );
+    return <div className="absolute inset-0 bg-no-repeat bg-bottom bg-cover bg-[url('/background.svg')]" />;
 };
 
 export default React.memo(BackgroundPattern);
