644e978f1621de84183e33eed3c9db0f8af1ca73
ferrodri <f.rodriguez.hervias@gmail.com>
2023-11-21 17:34:05 +0100

    z-index fix

--- src/app/components/landing/GlowButton.tsx
+++ src/app/components/landing/GlowButton.tsx
@@ -2,7 +2,7 @@ const GlowButton = () => {
     return (
         <a
             href="https://app.2718.co/"
-            className="hover:bg-[#313131] fixed bg-[#1a1a1a] bottom-4 sm:bottom-8 lg:bottom-12 right-[5vw] sm:right-[1%] sm:left-[1%] z-10 w-[90vw] max-w-[488px] flex justify-center mt-4 lg:my-0 mx-auto lg:w-auto lg:max-w-[488px] rounded-2xl border-2 border-highlight lg:px-14 py-4"
+            className="hover:bg-[#313131] fixed bg-[#1a1a1a] bottom-4 sm:bottom-8 lg:bottom-12 right-[5vw] sm:right-[1%] sm:left-[1%] z-20 w-[90vw] max-w-[488px] flex justify-center mt-4 lg:my-0 mx-auto lg:w-auto lg:max-w-[488px] rounded-2xl border-2 border-highlight lg:px-14 py-4"
         >
             <span className="lg:text-2xl font-bold uppercase text-white">Enter viator program</span>
         </a>
